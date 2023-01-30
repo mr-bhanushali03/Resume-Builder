9f6545e6e665b2fde585764579fa124c9f0386d6
mr-bhanushali03 <bhanushalikeval51@gmail.com>
2023-01-30 19:04:43 +0530

    try commit

--- relationship-crud/header.php
+++ relationship-crud/header.php
@@ -11,7 +11,7 @@
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato&display=swap">
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="style.css" /  >
 </head>
 
 <body>
